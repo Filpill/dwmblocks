f644d6779beb1a30816d1e5a049fb99de375d89e
filpill <filiplivancic@gmail.com>
2025-03-15 02:47:41 +0000

    replacing with nerd font icons

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"󰄨 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%Y %b %d (%a) | %H:%M'",					5,		0},
+	{" ", "date '+%Y %b %d (%a) |  %H:%M'",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
